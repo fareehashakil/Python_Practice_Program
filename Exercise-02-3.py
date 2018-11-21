3957a42ec3de38d69a6c97da66b2550febe6026b
Fareeha Shakil <44946231+fareehashakil@users.noreply.github.com>
2018-11-21 18:56:25 +0500

    Update Exercise-02-3.py

--- Exercise-02-3.py
+++ Exercise-02-3.py
@@ -1,3 +1,9 @@
+# Object:
+#   Take name as an input from user and a character to count how many
+#   times it will appear in his or her name.
+  
+  
+# Source Code:
 name, char = input("Enter your name and Enter character u want to count: ").split(" ")
 cou1 = name.count(char.upper())
 cou2 = name.count(char.lower())
