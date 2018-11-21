37a45b59b0e3e5865f10d3abc3b415fd61d328a8
Fareeha Shakil <44946231+fareehashakil@users.noreply.github.com>
2018-11-21 18:53:45 +0500

    Update Exercise-02-2.py

--- Exercise-02-2.py
+++ Exercise-02-2.py
@@ -1,3 +1,9 @@
+# Object:
+#     To take user's name and age input and allow user to watch movie 
+#     if the first alphabet of user's name is 'A' or 'a' 
+#     and age is greater than 10 by using If and Else statements.
+
+
 name, age = input("Enter name and age: ").split(" ")
 namee = name.lower()
 pos = namee.find("a")
