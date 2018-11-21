acfcb3933067a2818c5b43bf8b59a395905eaf1c
Fareeha Shakil <44946231+fareehashakil@users.noreply.github.com>
2018-11-21 23:24:13 +0500

    Update Exercise-04-2.py

--- Exercise-04-2.py
+++ Exercise-04-2.py
@@ -1,3 +1,15 @@
+Object:
+    To chek whether the entered string is a palindrome or not.
+    
+    
+    Palindrome:
+        Palindrome is a word which spell same in forward as well as reverse order.
+        E.g. madam, afifa etc. 
+
+    
+    
+    
+Sourcce Code:
 def Palindrome(str):
     name = str[::-1]
     if name == str:
