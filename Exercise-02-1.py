c13b2d3a57d87384b2d8edbcc3fb595456b3a54c
Fareeha Shakil <44946231+fareehashakil@users.noreply.github.com>
2018-11-21 18:50:49 +0500

    Update Exercise-02-1.py

--- Exercise-02-1.py
+++ Exercise-02-1.py
@@ -1,3 +1,9 @@
+# Object:
+#   Take 3 input in a single line then calculate the average of them and print it using formatting 3.6.
+
+
+
+# Source Code:
 val1, val2, val3 = input("Enter 3 values for taking average : ").split(" ")
 val = (int(val1) + int(val2) + int(val3) ) // 3
 print(f"The average of {val1} , {val2} and {val3} is {val}")
