3e67a1b4a94247c5d705a8dccd2bcacc8aee0fd1
Fareeha Shakil <44946231+fareehashakil@users.noreply.github.com>
2018-11-21 18:43:46 +0500

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+def fib(n):
+    a = 0
+    b = 1
+    if n == 0:
+        pass
+    elif n == 1:
+        print(a)
+    elif n == 2:
+        print(a , b)
+    else:
+        print(a , b , end = " ")
+        for i in range(n-2):
+            c = a + b
+            a = b
+            b = c
+            print(b , end = " ")
+
+fib(int(input("Enter no: ")))
