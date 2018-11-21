73705fd8f2b0f01cbbc8a414d45f60e9b9c056e0
Fareeha Shakil <44946231+fareehashakil@users.noreply.github.com>
2018-11-21 18:59:07 +0500

    Update Exercise-03-1.py

--- Exercise-03-1.py
+++ Exercise-03-1.py
@@ -1,3 +1,10 @@
+# Object:
+#   Lets play a guessing game using nested if statement!
+#   Ask the user to enter any number and if its the winning number
+#   print You win!!
+  
+  
+# Source Code:
 winning_no = 12
 no = int(input("Enter any number for guessing game: "))
 if no == winning_no:
