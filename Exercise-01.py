bf334de405a1b75d0d5985bd22983b1ad4688709
Fareeha Shakil <44946231+fareehashakil@users.noreply.github.com>
2018-11-21 18:48:38 +0500

    Update Exercise-01.py

--- Exercise-01.py
+++ Exercise-01.py
@@ -1,3 +1,12 @@
+# Object:
+#    To print using Escape Sequence!
+#     this is \\\\ double backslash
+#     this is /\\/\\/\\/\\/\\ mountain
+#     he is \tawesome
+#     \\\" \\n \\t \\\'
+
+
+
 print("this is \\\\ double backslash")
 print("this is /\\/\\/\\/\\/\\ mountain")
 print("he is \tawesome ")
